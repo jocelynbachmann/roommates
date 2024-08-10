4a2628e0998ffa574a4d583779417848901a718a
jocelynbachmann <82013530+jocelynbachmann@users.noreply.github.com>
2024-08-09 22:57:08 -0700

    Fix server loading message logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,42 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import Layout from './Layout';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { getUsersAsync } from './redux/users/thunks';
 import { getGroupsAsync } from './redux/groups/thunks';
-import { REQUEST_STATE } from './redux/utils';
 import { PageLoader } from './components/Auth/PageLoader';
 
 export default function App() {
   const dispatch = useDispatch();
-  const groupsStatus = useSelector(state => state.groups.getGroups);
+  const [isLoading, setIsLoading] = useState(true);
   const [isTakingTooLong, setIsTakingTooLong] = useState(false);
 
   useEffect(() => {
-    dispatch(getGroupsAsync());
-    dispatch(getUsersAsync());
+    const fetchInitialData = async () => {
+      try {
+        await dispatch(getGroupsAsync()).unwrap();
+        await dispatch(getUsersAsync()).unwrap();
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Failed to fetch initial data', error);
+      }
+    };
+
+    fetchInitialData();
 
     const timeout = setTimeout(() => {
-      setIsTakingTooLong(true);
+      if (isLoading) {
+        setIsTakingTooLong(true);
+      }
     }, 10000);
 
     return () => clearTimeout(timeout);
-  }, [dispatch]);
+  }, [dispatch, isLoading]);
 
-  if (groupsStatus === REQUEST_STATE.IDLE || groupsStatus === REQUEST_STATE.PENDING) {
+  if (isLoading) {
     return <PageLoader isTakingTooLong={isTakingTooLong} />;
-  } 
+  }
 
-  return (
-    <Layout />
-  );
+  return <Layout />;
 }
+
