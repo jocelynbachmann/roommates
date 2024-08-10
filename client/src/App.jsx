4e2b42b58ba639b23384463c5b8fd72f3883b698
jocelynbachmann <82013530+jocelynbachmann@users.noreply.github.com>
2024-08-09 22:15:02 -0700

    Add server loading message

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,31 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Layout from './Layout';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getUsersAsync } from './redux/users/thunks';
 import { getGroupsAsync } from './redux/groups/thunks';
+import { REQUEST_STATE } from './redux/utils';
+import { PageLoader } from './components/Auth/PageLoader';
 
 export default function App() {
   const dispatch = useDispatch();
+  const groupsStatus = useSelector(state => state.groups.getGroups);
+  const [isTakingTooLong, setIsTakingTooLong] = useState(false);
 
   useEffect(() => {
     dispatch(getGroupsAsync());
     dispatch(getUsersAsync());
+
+    const timeout = setTimeout(() => {
+      setIsTakingTooLong(true);
+    }, 10000);
+
+    return () => clearTimeout(timeout);
   }, [dispatch]);
+
+  if (groupsStatus === REQUEST_STATE.IDLE || groupsStatus === REQUEST_STATE.PENDING) {
+    return <PageLoader isTakingTooLong={isTakingTooLong} />;
+  } 
 
   return (
     <Layout />
--- client/src/components/Auth/PageLoader.jsx
+++ client/src/components/Auth/PageLoader.jsx
@@ -1,7 +1,7 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Text } from '@chakra-ui/react';
 import { Circles } from 'react-loader-spinner';
 
-export const PageLoader = () => {
+export const PageLoader = ({ isTakingTooLong = false }) => {
   return (
     <Box
       position="absolute"
@@ -13,6 +13,7 @@ export const PageLoader = () => {
       display="flex"
       alignItems="center"
       justifyContent="center"
+      flexDirection="column"
     >
       <Circles
         height="80"
@@ -21,6 +22,11 @@ export const PageLoader = () => {
         ariaLabel="circles-loading"
         visible={true}
       />
+      {isTakingTooLong && (
+        <Text mt={6} color="teal" fontSize="lg" fontWeight="bold" textAlign="center">
+          The server is taking longer than usual to respond. This may be due to the server spinning up after being idle. Please wait a moment.
+        </Text>
+      )}
     </Box>
   );
 };
